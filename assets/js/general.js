18b393d0d83a0974257342075a87c117ea6b5ca1
LizzieeLovett <lizzie.m.lovett@gmail.com>
2021-06-19 16:48:06 +0000

    Added functionality to form submit button on contact.html to check for type mismatched inputs via general.js

--- assets/js/general.js
+++ assets/js/general.js
@@ -38,11 +38,12 @@ function validationCheck() {
     let failCheck
     for(let i = 0; i < validationNullCheck.length; i++)
     {
-        if(validationNullCheck[i].value == null || validationNullCheck[i].value == "" || validationNullCheck[i].value == "Select...") {
+        if(validationNullCheck[i].value == null || validationNullCheck[i].value == "" || validationNullCheck[i].value == "Select..." || validationNullCheck[i].validity.typeMismatch) {
             failCheck = "Fail"
-        }
+        } 
     }
     if (failCheck=="Fail") {
+        event.preventDefault()
         document.getElementById("submit-modal-text").innerHTML = "Sorry, something on your form doesn't look right. Please review your answers and try to submit again."
     } else {
         document.getElementById("submit-modal-text").innerHTML = "Thank you for your submission! We'll be in contact with you regarding your message as soon as we can."
