3ee3df0888836fd1ccdb721aae296f6e7d4dc3eb
LizzieeLovett <lizzie.m.lovett@gmail.com>
2021-06-19 14:15:14 +0000

    Developed the share modal and added js to copy the link to the clipboard

--- assets/js/general.js
+++ assets/js/general.js
@@ -1,10 +1,28 @@
 let openShareId = document.getElementById("open-share");
 let closeShareId = document.getElementById("close-share");
 let containerShareId = document.getElementById("share-container");
+let copyShareId = document.getElementById("share-copy");
+let copyTextShareId = document.querySelector("#share-modal-text");
 
-openShareId.addEventListener("click", toggleHideShare);
+openShareId.addEventListener("click", openShare);
 closeShareId.addEventListener("click", toggleHideShare);
+copyShareId.addEventListener("click", copyToClipboard);
+
+function openShare() {
+    document.getElementById("share-modal-text").innerHTML = window.location.protocol + "//" + window.location.host + "/index.html"
+    toggleHideShare()
+}
 
 function toggleHideShare() {
     containerShareId.classList.toggle("hide-me");
 }
+
+// https://www.w3schools.com/howto/howto_js_copy_clipboard.asp to help me
+function copyToClipboard() {
+    document.getElementById("share-modal-text").innerHTML = "<textarea id='myTextArea'>" + window.location.protocol + "//" + window.location.host + "/index.html</textarea>"
+    document.getElementById("myTextArea").select();
+    document.execCommand("copy");
+    document.getElementById("share-modal-text").innerHTML = window.location.protocol + "//" + window.location.host + "/index.html"
+    alert("Link copied to clipboard");
+}
+
